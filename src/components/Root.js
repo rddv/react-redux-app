035f1e71bc0530151a1f14cb6b648d94adb17705
Denis Razumnyi <rddv88@gmail.com>
2018-05-22 16:00:58 +0300

    init

---
+++ src/components/Root.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Root extends Component {
+    static propTypes = {};
+
+    render() {
+        return (
+            <div>
+                <h1>Hello </h1>
+            </div>
+        );
+    }
+}
+
+export default Root;
